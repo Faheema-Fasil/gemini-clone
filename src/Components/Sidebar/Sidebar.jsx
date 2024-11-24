8e019033b3d4c0f075e60e127615f9f4a4246976
Faheema_Fasil <faheemafasil12@gmail.com>
2024-11-24 18:06:52 +0530

    progress

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Sidebar.css'
 import { assets  } from '../../assets/assets'
+import { Context } from '../../context/Context'
 function Sidebar() {
   const [extended,setExtented]=useState(false)
+  const {onSent,prevPrompt,setRecentPrompt,newChat}=useContext(Context)
+  const loadPrompt=async (prompt)=>{
+    setRecentPrompt(prompt)
+   await onSent(prompt)
+  }
   return (
     <div className='sidebar'>
       <div className="top">
         <img className='menu' onClick={()=>setExtented(prev=>!prev )} src={assets.menu_icon} alt="" />
-        <div className="new-chat">
+        <div onClick={()=>newChat()} className="new-chat">
             <img src={assets.plus_icon} alt="" />
            {extended? <p>New Chat</p>:null}
         </div>
@@ -17,10 +23,15 @@ function Sidebar() {
           <p className='recent-title'>
               Recent
           </p>
-          <div className="recent-entry">
+          {prevPrompt.map((item ,index)=>{
+            return (
+              <div onClick={()=>loadPrompt(item)} className="recent-entry">
               <img src={assets.message_icon} alt="" />
-              <p>What is react...</p>
+              <p>{item.slice(0,18)}...</p>
           </div>
+            )
+          })}
+          
       </div>
       :null
         }
--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -1,1 +1,36 @@
-const apiKey= "AIzaSyB_IA66A0Yr32YMmT_LJvq1Ty_ALEFq6pc";
+const GEMINI_API_KEY= "AIzaSyB_IA66A0Yr32YMmT_LJvq1Ty_ALEFq6pc";
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  }  from "@google/generative-ai";
+  
+  const apiKey = GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text()
+  }
+  
+  export default run;
